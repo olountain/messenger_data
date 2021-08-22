8b7a42edbaba638733507896ace5d043b38f80b5
olountain <o.lountain@gmail.com>
2021-08-22 22:56:55 +0930

    changing port

--- run.R
+++ run.R
@@ -2,6 +2,6 @@ pacman::p_load(shiny)
 port <- Sys.getenv('PORT')
 shiny::runApp(
     appDir = "app.R",
-    host = '0.0.0.0',
-    port = as.numeric(port)
+    # host = '0.0.0.0',
+    # port = as.numeric(port)
 )
